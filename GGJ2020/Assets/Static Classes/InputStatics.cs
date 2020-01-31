54b8efd919eeb22e2d52c6a62f8f800acf76615f
Filip Petersson <zmarfan@hotmail.com>
2020-02-01 00:01:26 +0100

    Started on pickup system

---
+++ GGJ2020/Assets/Static Classes/InputStatics.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputStatics
+{
+    public static readonly string HORIZONTAL = "Horizontal";
+    public static readonly string VERTICAL = "Vertical";
+
+    public static readonly string FIRE_1 = "Fire1";
+    public static readonly string FIRE_2 = "Fire2";
+}
