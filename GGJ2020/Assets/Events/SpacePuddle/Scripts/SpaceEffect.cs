81bec8cd8e5c70bcee4a94fd52dc349ebeabccb7
Unknown <dane.nilsson@gmail.com>
2020-02-01 15:06:32 +0100

    Finished space puddle code and added graphics

---
+++ GGJ2020/Assets/Events/SpacePuddle/Scripts/SpaceEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceEffect : MonoBehaviour
+{
+    [SerializeField] string playerTag;
+    [SerializeField] float floatTime;
+    [SerializeField] float bounceForce;
+    [SerializeField] float torqueForce;
+
+
+    private void OnTriggerEnter(Collider hit)
+    {
+        if (hit.tag == playerTag)
+        {
+            hit.GetComponent<eventEffects>().SpacePuddleEffect(floatTime);
+            hit.GetComponent<Rigidbody>().AddForce(Vector3.up * bounceForce);
+            Vector3 torqueVector = hit.transform.position - this.transform.position;
+            hit.GetComponent<Rigidbody>().AddTorque(torqueVector * torqueForce);
+        }
+    }
+}
---
+++ GGJ2020/Assets/Player/Scripts/Gravity.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    [SerializeField] private float gravityModifier;
+
+
+    private bool _useGravity;
+    private Rigidbody _rb;
+    private Vector3 _prevPosition;
+    private Vector3 _newPosition;
+    private Vector3 _objectVelocity;
+
+    public bool UseGravity { get => _useGravity; set => _useGravity = value; }
+    public Vector3 ObjectVelocity { get => _objectVelocity; set => _objectVelocity = value; }
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _useGravity = true;
+    }
+
+    private void Start()
+    {
+        _prevPosition = transform.position;
+        _newPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        _newPosition = transform.position; 
+        ObjectVelocity = (_newPosition - _prevPosition) / Time.fixedDeltaTime;  
+        _prevPosition = _newPosition;  
+
+        if (_useGravity)
+        {
+            _rb.AddForce(new Vector3(0, -gravityModifier, 0));
+
+            if (ObjectVelocity.y < 0)
+            {
+                _rb.AddForce(new Vector3(0, -gravityModifier, 0) * (_rb.mass * _rb.mass));
+            }
+        }    
+    }
+}
